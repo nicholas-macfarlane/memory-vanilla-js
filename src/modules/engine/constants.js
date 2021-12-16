130721804fef86039770e39567ff39bc2ead5bac
nicholas-macfarlane <nmacfarlane110@gmail.com>
2021-12-16 18:44:01 +0200

    modularizing css

--- src/modules/engine/constants.js
+++ src/modules/engine/constants.js
@@ -11,5 +11,6 @@ export const append = (element)=> {
 export const set = (element, attribute, value)=> {
     element.setAttribute(attribute, value);
 }
-
-
+//links
+export const codePen = ()=> "https://codepen.io/nmacfarlane/full/abOrWRy";
+export const gitHub = ()=> "https://github.com/nicholas-macfarlane/memory-vanilla-js";
